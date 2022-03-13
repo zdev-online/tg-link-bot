a62dc4ca5d029a67df2073b77cdccd8456dff42d
Zharckov <23assasyncred03@gmail.com>
2022-03-13 12:48:59 +0300

    fixes

--- src/handlers/channel-stats.handler.ts
+++ src/handlers/channel-stats.handler.ts
@@ -1,10 +1,11 @@
 import { Telegraf } from "telegraf";
 import { BotContext } from "../interfaces";
+import { GET_STATS } from "../keyboard.buttons";
 import { CHANNEL_STATS_SCENE } from "../scene.ids";
 import { is_admin, logger } from "../utils";
 
 export const channel_stats_handler = (bot: Telegraf<BotContext>) => {
-  bot.start(async ctx => {
+  bot.hears(GET_STATS, async ctx => {
     if (!is_admin(ctx.from.id)) { return; }
     return await ctx.scene.enter(CHANNEL_STATS_SCENE);
   });
